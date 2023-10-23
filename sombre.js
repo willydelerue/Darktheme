28f2c2c33681079610ec20ced40d8544fbe1818e
willydelerue <willy.delerue@gmail.com>
2023-10-23 15:49:41 +0200

    étape 2 ajout du clic et vérif si classe dark existe

---
+++ sombre.js
@@ -0,0 +1,6 @@
+let btn  = document.querySelector('#mode');
+let span = document.querySelector('span');
+
+btn.addEventListener('click', (e) => {
+  if(document.body.classList.contains('dark'));
+})

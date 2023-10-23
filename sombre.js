adf57723e693ef0820450ef082066a5d404605c8
willydelerue <willy.delerue@gmail.com>
2023-10-23 16:04:04 +0200

    création de la fonction modeSombre

--- sombre.js
+++ sombre.js
@@ -8,9 +8,16 @@ let span = document.querySelector('span');
 btn.addEventListener('click', () => {
   if(document.body.classList.contains('dark')) {
     span.textContent = "Thème sombre";
-    localStorage = ('theme', 'clair');
+    localStorage.setItem = ('theme', 'clair');
 } 
 else {
     modeSombre();
 }
 });
+
+function modeSombre() {
+    document.body.className = "dark";
+    span.textContent = "Thème clair";
+    localStorage.setItem = ('theme', 'sombre');
+
+}

650f878775c25e3600cf44beba874694d074f491
willydelerue <willy.delerue@gmail.com>
2023-10-23 19:31:07 +0200

    derniers ajustements

--- sombre.js
+++ sombre.js
@@ -1,28 +1,25 @@
-let btn  = document.querySelector('#mode');
-let span = document.querySelector('span');
+let btn = document.querySelector('#mode');
+let span   = document.querySelector('span');
 
-if (localStorage.getItem('theme')) {
-    if (localStorage.getItem('theme') == 'sombre') {
-        modeSombre();
-    }
+if(localStorage.getItem('theme')){
+  if(localStorage.getItem('theme') == 'sombre') {
+    modeSombre();
+  }
 }
-
-
 
 btn.addEventListener('click', () => {
   if(document.body.classList.contains('dark')) {
     document.body.className = '';
-    span.textContent = "Thème sombre";
-    localStorage.setItem = ('theme', 'clair');
-} 
-else {
+    span.textContent = 'Thème sombre';
+    localStorage.setItem('theme', 'clair');
+  }
+  else {
     modeSombre();
-}
+  }
 });
 
 function modeSombre() {
-    document.body.className = "dark";
-    span.textContent = "Thème clair";
-    localStorage.setItem = ('theme', 'sombre');
-
+  document.body.className = 'dark';
+  span.textContent = 'Thème clair';
+  localStorage.setItem('theme', 'sombre');
 }

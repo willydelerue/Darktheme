6c385aa5cb1514ff1bf5c6ddce0a1e3d4d7b52bb
willydelerue <willy.delerue@gmail.com>
2023-10-23 16:16:05 +0200

    Regarder si la préférence de l'utilisateur existe déjà

--- sombre.js
+++ sombre.js
@@ -1,12 +1,17 @@
 let btn  = document.querySelector('#mode');
 let span = document.querySelector('span');
 
-
+if (localStorage.getItem('theme')) {
+    if (localStorage.getItem('theme') == 'sombre') {
+        modeSombre();
+    }
+}
 
 
 
 btn.addEventListener('click', () => {
   if(document.body.classList.contains('dark')) {
+    document.body.className = '';
     span.textContent = "Thème sombre";
     localStorage.setItem = ('theme', 'clair');
 } 
